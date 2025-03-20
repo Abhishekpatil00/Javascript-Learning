2d83bb9734d08d8bae298a6bc9ca2b2041e3dd43
Abhishekpatil00 <75978000+Abhishekpatil00@users.noreply.github.com>
2025-03-20 18:50:47 +0000

    conversioOperation

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,51 @@
+let score = "8987yu";
+
+let valueNumber = Number(score);
+
+console.log(typeof(valueNumber));
+console.log(valueNumber);
+
+let score1 = null;
+
+let valueNumber1 = Number(score1);
+
+console.log(typeof(valueNumber1));
+console.log(valueNumber1);
+
+let score2 = undefined;
+
+let valueNumber2 = Number(score2);
+
+console.log(typeof(valueNumber1));
+console.log(valueNumber2);
+
+let score3 = true;
+
+let valueNumber3 = Number(score1);
+
+console.log(typeof(valueNumber3));
+console.log(valueNumber3);
+
+let score4 = "abhishek";
+
+let valueNumber4 = Number(score4);
+
+console.log(typeof(valueNumber4));
+console.log(valueNumber4);
+
+let isLoggedIn = 1;
+
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+console.log(typeof(booleanIsLoggedIn));
+console.log(booleanIsLoggedIn);
+
+// 1=>true
+// "" => false
+// "abhi" => true
+
+let someNumber = 56
+let stringNumber = String(someNumber)
+
+console.log(typeof stringNumber);
+console.log(stringNumber);
+
